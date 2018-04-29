901a2009911d808e0d26a352e8e8b4efb63cb40d
Endalth <alican__karamaras@hotmail.com>
2018-04-30 00:07:24 +0300

    Change profile picture

--- app/src/main/java/com/seniorproject/uninet/uninet/Settings/ProfileSettingsFragment.java
+++ app/src/main/java/com/seniorproject/uninet/uninet/Settings/ProfileSettingsFragment.java
@@ -1,6 +1,7 @@
 package com.seniorproject.uninet.uninet.Settings;
 
 import android.app.AlertDialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -23,8 +24,11 @@ import android.widget.Toast;
 
 import com.seniorproject.uninet.uninet.R;
 import com.seniorproject.uninet.uninet.StoredUserInformation;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.Objects;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -42,14 +46,15 @@ public class ProfileSettingsFragment extends Fragment {
     EditText webPage;
     TextView changeProfilePicture;
     Button setRelationship;
-    byte[] myImage;
+    byte[] bigProfilePictureImage;
+    byte[] smallProfilePictureImage;
+    String imagePath = "";
 
     StoredUserInformation userInformation;
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View profileSettingsView = inflater.inflate(R.layout.fragment_profile_settings, container, false);
         Log.d(TAG, "OnCreateView: ProfileSettingsFragment");
         return profileSettingsView;
@@ -62,7 +67,6 @@ public class ProfileSettingsFragment extends Fragment {
         Log.d(TAG, "onActivityCreated: ProfileSettingsFragment");
 
         userInformation = new StoredUserInformation(Objects.requireNonNull(getContext()));
-
 
 
         backArrowButton = (getActivity()).findViewById(R.id.back_arrow_button);
@@ -109,53 +113,70 @@ public class ProfileSettingsFragment extends Fragment {
         changeProfilePicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                startActivityForResult(intent, 9);
-
-
+                performCrop();
             }
         });
 
 
     }
 
+    private void performCrop() {
+        CropImage.activity().start(getContext(), this);
+    }
+
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(requestCode == 9 && resultCode == RESULT_OK && data != null){
-            Uri pickedImage = data.getData();
-            String[] filePath = {MediaStore.Images.Media.DATA};
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
 
-            Cursor cursor = getContext().getContentResolver().query(pickedImage, filePath, null, null, null);
-            cursor.moveToFirst();
+                Uri resultUri = result.getUri();
+                Uri originalUri = result.getOriginalUri();
 
-            String imagePath = cursor.getString(cursor.getColumnIndex(filePath[0]));
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            BitmapFactory.decodeFile(imagePath).compress(Bitmap.CompressFormat.PNG, 72, stream);
-            myImage = stream.toByteArray();
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                Bitmap bitmap = null;
+                try {
+                    bitmap = MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), resultUri);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 72, stream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                smallProfilePictureImage = stream.toByteArray();
 
-            Bitmap bitmap = BitmapFactory.decodeByteArray(myImage, 0, myImage.length);
-            CircleImageView imageView = (CircleImageView)getActivity().findViewById(R.id.profile_photo);
+                CircleImageView imageView = (CircleImageView) getActivity().findViewById(R.id.profile_photo);
 
-            imageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, imageView.getWidth(), imageView.getHeight(), false));
+                imageView.setImageBitmap(Bitmap.createScaledBitmap(bitmap, imageView.getWidth(), imageView.getHeight(), false));
 
-            cursor.close();
+                stream = new ByteArrayOutputStream();
+                try {
+                    MediaStore.Images.Media.getBitmap(getActivity().getContentResolver(), originalUri).compress(Bitmap.CompressFormat.PNG, 72, stream);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                bigProfilePictureImage = stream.toByteArray();
+
+                int a = 0;
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 
-    private void Save()
-    {
-        String imageString = "";
+    private void Save() {
+        String bigImageString = "";
+        String smallImageString = "";
 
-        for(int i = 0; i < myImage.length; i++){
-            imageString += i < myImage.length - 1 ? (myImage[i] & 0xFF) + "," : (myImage[i] & 0xFF);
+        for (int i = 0; i < bigProfilePictureImage.length; i++) {
+            bigImageString += i < bigProfilePictureImage.length - 1 ? (bigProfilePictureImage[i] & 0xFF) + "," : (bigProfilePictureImage[i] & 0xFF);
+        }
+
+        for (int i = 0; i < smallProfilePictureImage.length; i++) {
+            smallImageString += i < smallProfilePictureImage.length - 1 ? (smallProfilePictureImage[i] & 0xFF) + "," : (smallProfilePictureImage[i] & 0xFF);
         }
     }
 
-    private void setRelationship()
-    {
+    private void setRelationship() {
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
         //builder.setTitle(R.string.pref_title_relationshipStatus);
         builder.setTitle(R.string.pref_title_relationshipStatus);
@@ -170,8 +191,6 @@ public class ProfileSettingsFragment extends Fragment {
             }
         });
         builder.show();
-
-
 
 
     }
